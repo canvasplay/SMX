d25cf17742f43347cb3b39c2aae8b0b3ec57bd36
clopezdepablo <clopezdepablo@gmail.com>
2016-11-24 23:30:00 +0100

    updated according to new source code

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,67 +1,120 @@
 module.exports = function(grunt) {
 
-  // Project configuration.
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-    concat: {
-      options: {
-        // define a string to put between each file in the concatenated output
-        separator: ';'
-      },
-      dist: {
-        // the files to concatenate
-        src: [
-          '<%= pkg.branch %>/smx.js',
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
 
-          '<%= pkg.branch %>/compiler/Compiler.js',
+        bump: {
+          options: {
+            files:  ['package.json'],
+            updateConfigs: ['pkg'],
+            commit: false,
+            createTag: false,
+            push: false
+          }
+        },
 
-          '<%= pkg.branch %>/time/time.js',
-          '<%= pkg.branch %>/time/Timer.js',
-          '<%= pkg.branch %>/time/Timeline.js',
-          '<%= pkg.branch %>/playhead/Playhead.js',
+        babel: {
+            options: {
+                sourceMap: true,
+                presets: ['babel-preset-es2015']
+            },
+            dist: {
+                files: [{
+                    expand: true,
+                    cwd: 'src/',
+                    src: ['**/*.js'],
+                    dest: 'build/'
+                }]
+            },
 
-          '<%= pkg.branch %>/tracking/tracking.js',
-          '<%= pkg.branch %>/tracking/attributes/attributes.js',
-          '<%= pkg.branch %>/tracking/attributes/progress.js',
-          '<%= pkg.branch %>/tracking/attributes/score.js',
-          '<%= pkg.branch %>/tracking/TriggerExpression.js',
-          '<%= pkg.branch %>/tracking/Track.js',
-          '<%= pkg.branch %>/tracking/TrackManager.js',
+        },
 
-          '<%= pkg.branch %>/meta/meta.js',
-          '<%= pkg.branch %>/meta/MetaData.js',
-          '<%= pkg.branch %>/meta/MetaManager.js',
+        concat: {
+            options: {
+                separator: ';'
+            },
+            dist: {
+                src: [
 
-          '<%= pkg.branch %>/document/Sizzle.js',
-          '<%= pkg.branch %>/document/CSSParser.js',
-          '<%= pkg.branch %>/document/TimeAttrController.js',
-          '<%= pkg.branch %>/document/UIAttrController.js',
-          '<%= pkg.branch %>/document/Document.js'
+                    'build/document/Sizzle.js',
+
+                    'build/smx.js',
+
+                    'build/compiler/Compiler.js',
+
+                    'build/time/time.js',
+                    'build/time/Timer.js',
+                    'build/time/Timeline.js',
+                    'build/playhead/SMXPlayhead.js',
 
 
-        ],
-        // the location of the resulting JS file
-        dest: 'build/smx.js'
-      }
-    },
-    uglify: {
-      options: {
-        // the banner is inserted at the top of the output
-        banner: '/*! <%= pkg.name %> <%= pkg.branch %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
-      },
-      dist: {
-        files: {
-          'build/smx.min.js': ['<%= concat.dist.dest %>']
-        }
-      }
-    }
-  });
 
-  grunt.loadNpmTasks('grunt-contrib-concat');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
+                    'build/finder/SMXFinder.js',
 
-  // Default task(s).
-  grunt.registerTask('default', ['concat','uglify']);
-  //grunt.registerTask('default', ['concat']);
+
+                    'build/document/fn.js',
+                    'build/document/TimeAttrController.js',
+                    'build/document/UIAttrController.js',
+
+
+                    //PLUGINS
+
+                    //Tracking
+                    'build/tracking/TrackManager.js',
+                    'build/tracking/attributes/progress.js',
+                    'build/tracking/attributes/score.js',
+                    'build/tracking/attributes/status.js',
+                    'build/tracking/attributes/access.js',
+                    'build/tracking/attributes/points.js',
+                    'build/tracking/TrackInterface.js',
+                    
+
+                    //Prototype
+                    'build/plugins/prototype/CSSParser.js',
+                    'build/plugins/prototype/PrototypeParser.js',
+                    
+                    //Metadata
+                    'build/plugins/metadata/MetadataParser.js',
+                    'build/plugins/metadata/MetadataInterface.js',
+
+                    //Taxonomy
+                    //'build/taxonomy/TaxonomyParser.js',
+                    'build/plugins/taxonomy/TaxonomyInterface.js',
+
+
+                    'build/document/Document.js'
+
+
+                ],
+                dest: 'build/smx.js'
+            }
+        },
+
+        uglify: {
+            options: {
+                banner: '/*! <%= pkg.name %> <%= pkg.version %> <%= grunt.template.today("dd-mm-yyyy HH:MM:ss") %> */\n'
+            },
+            dist: {
+                files: {
+                    'build/smx.min.js': ['<%= concat.dist.dest %>']
+                }
+            }
+        },
+
+        //clean all subfolders in build/
+        clean: ['build/*/']
+
+
+    });
+
+    grunt.loadNpmTasks('grunt-bump');
+    grunt.loadNpmTasks('grunt-babel');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+
+    grunt.registerTask('version', ['bump']);
+    //grunt.registerTask('default', ['babel', 'concat', 'uglify','clean']);
+    grunt.registerTask('default', ['babel', 'concat', 'clean']);
 
 };
